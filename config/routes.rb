068de4957dec608b819851f87b93bc132e4df501
unknown <dhakadamit@.corporate.thoughtworks.com>
2010-02-05 19:22:57 +0530

    [Restfulx] : Generated model.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :tags
-
-  map.resources :notes
-
+  
+  # Map application root to default RestfulX controller
+  map.root :controller => "flex"
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -41,7 +41,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
+  # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end

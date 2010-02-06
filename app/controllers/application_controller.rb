c7e94f12a9394205c83c8fa400fa63650c8431c5
unknown <dhakadamit@.corporate.thoughtworks.com>
2010-02-07 00:24:24 +0530

    [Misc] : Defaulting dasherize to false so as to have xml elements with underscores and not hyphens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,49 @@
+module ActiveSupport #:nodoc:
+  module CoreExtensions #:nodoc:
+    module Hash #:nodoc:
+      module Conversions
+        # We force :dasherize to be false, since we never want
+        # it true.  Thanks very much to the reader on the
+        # flexiblerails Google Group who suggested this better
+        # approach.
+        unless method_defined? :old_to_xml
+          alias_method :old_to_xml, :to_xml
+          def to_xml(options = {})
+            options.merge!(:dasherize => false)
+            old_to_xml(options)
+          end
+        end
+      end
+    end
+    module Array #:nodoc:
+      module Conversions
+        # We force :dasherize to be false, since we never want
+        # it to be true.
+        unless method_defined? :old_to_xml
+          alias_method :old_to_xml, :to_xml
+          def to_xml(options = {})
+            options.merge!(:dasherize => false)
+            old_to_xml(options)
+          end
+        end
+      end
+    end
+  end
+end
+module ActiveRecord #:nodoc:
+  module Serialization
+    # We force :dasherize to be false, since we never want it to
+    # be true.
+    unless method_defined? :old_to_xml
+      alias_method :old_to_xml, :to_xml
+      def to_xml(options = {})
+        options.merge!(:dasherize => false)
+        old_to_xml(options)
+      end
+    end
+  end
+end
+
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
